bfea24ae18cc4a0ef9c42e56fda50cee1a7cb80d
itssthatamurky <heymarks@gmail.com>
2026-06-11 18:45:02 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const C = 'badgecap-v1';
+const SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-180.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c => c.addAll(SHELL)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k))))
+      .then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request).then(net => {
+    const cp = net.clone();
+    caches.open(C).then(c => c.put(e.request, cp));
+    return net;
+  }).catch(err => e.request.mode === 'navigate' ? caches.match('./index.html') : Promise.reject(err))));
+});
